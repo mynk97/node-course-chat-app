78ed679b6a81192aede0ff30d12f6bd2ca882825
arun.tanwar3 <arun.tanwar3@gmail.com>
2018-04-09 23:53:48 +0530

    Add new messageand create message events

---
+++ public/js/index.js
@@ -0,0 +1,28 @@
+var socket =io();
+  	  
+  	  socket.on('connect',function () {
+         console.log('Connected to server');
+     
+        /*socket.emit('createEmail',{
+            to: 'jen@example.com',
+            text: 'Hey. this is arun'
+        });*/
+
+        socket.emit('createMessage', {
+           to: 'aanand',
+            text: 'hey from this side'
+        });
+  	  });
+
+
+         socket.on('disconnect',function () {
+         	console.log('Disconnected to server');
+         });
+
+  	  /*socket.on('newEmail',function (email) {
+         console.log('New email',email);
+  	  });*/
+
+  	  socket.on('newMessage',function(message) {
+           console.log('New message', message);
+  	  });
--- server/server.js
+++ server/server.js
@@ -1,12 +1,49 @@
 const path=require('path');
+const http=require('http');
 const express=require('express');
+const socketIO=require('socket.io');
+
 const publicPath =path.join(__dirname,'../public');
+const port= process.env.PORT || 3000;
 
 var app=express();
+var server=http.createServer(app);
+var io=socketIO(server);
 
 app.use(express.static(publicPath));
+ 
+ io.on('connection',(socket)=> {
+ 	console.log('New user connected');
 
-app.listen(3000,()=> {
+   /*socket.emit('newEmail', {
+   	from :"jen@example.com",
+   	text: "Hey! wats goin on",
+   	createAt: 123
+   });
+*/  
+   socket.emit('newMessage', {
+      from: "mayank",
+      text: "hey buddy",
+      createAt: 123
+   });
+
+   /*socket.on('createEmail',(newEmail)=> {
+      console.log('createEmail',newEmail);
+   });*/
+
+   socket.on('createMessage', (newMessage)=> {
+       console.log('createMessage', newMessage);
+   });
+
+ 	socket.on('disconnect',()=> {
+   console.log('User was diconnected');
+ });
+
+ });
+
+ 
+
+server.listen(3000,()=> {
   console.log('Server is up on port 3000');
 });
 /*console.log(__dirname+'/../public');
