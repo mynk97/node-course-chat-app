682284000d4f6859800cb3264750523978a44d7a
arun.tanwar3 <arun.tanwar3@gmail.com>
2018-04-11 17:54:13 +0530

    Add css for chat page

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http=require('http');
 const express=require('express');
 const socketIO=require('socket.io');
 
-const {generateMessage}=require('./utils/message');
+const {generateMessage,generateLocationMessage}=require('./utils/message');
 const publicPath =path.join(__dirname,'../public');
 const port= process.env.PORT || 3000;
 
@@ -42,12 +42,16 @@ app.use(express.static(publicPath));
 
 
        io.emit('newMessage',generateMessage(Message.from,Message.text)); 
-        callback('Thos is from server');
+        callback();
        /*socket.broadcast.emit('newMessage',{
           from:Message.from,
           text: Message.text,
           createdAt: new Date().getTime() 
        });*/
+   });
+
+   socket.on('createLocationMessage', (coords)=> {
+     io.emit('newLocationMessage',generateLocationMessage('Admin',coords.latitude,coords.longitude));
    });
 
  	socket.on('disconnect',()=> {
